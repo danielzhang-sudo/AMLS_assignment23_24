05f350c9cda4a19eee9afde1065944722b9d3535
Xiaojun Daniel Zhang Chen <xdz037@cs.bham.ac.uk>
2024-01-12 23:49:35 +0000

    change filename

--- AMLS_23-24_SN23135632/main.py
+++ AMLS_23-24_SN23135632/main.py
@@ -1,10 +1,24 @@
-from A import acode
-from B import bcode
+import argparse
+from A import task_A
+from B import task_B
 
-def main():
-    acode.main()
+def main(args):
+    task = args.task
 
-    # bcode.main()
+    if task=="A":
+        task_A.main(args)
+    elif task=="B":
+        task_B.main(args)
+    else:
+        print("Run with the correct arguments")
+        raise 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description = 'Arguments')
+    parser.add_argument('--task', type=str, help='Task A or B')
+    parser.add_argument('--mode', type=str, default="testing", help='training or testing mode')
+    parser.add_argument('--alg', type=str, default="nb", help="algorithm to use")
+    parser.add_argument('--ckpt_path', type=str, default="", help='path of weights file')
+    args = parser.parse_args()
+
+    main(args)
